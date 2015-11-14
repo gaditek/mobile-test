f3143f93e1cbc155dca584129bdd6454ce19dfd7
Masoor ahmed <masroor.ahmed@gaditek.com>
2015-11-14 16:56:26 +0500

    initial commit

---
+++ PureVPNTest/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PureVPNTest
+{
+    class Utils
+    {
+        public static void writeToFile(string text)
+        {
+            // WriteAllText creates a file, writes the specified string to the file,
+            // and then closes the file.    You do NOT need to call Flush() or Close().
+            System.IO.File.WriteAllText(@"D:\PureVPNResult.txt", text);
+        }
+    }
+}
